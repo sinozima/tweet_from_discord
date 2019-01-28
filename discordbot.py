50ee01c9e9f6259ab3c3ad84d782828e679bef78
sinozima <150318gen@gmail.com>
2019-01-28 16:47:22 +0900

    second

---
+++ discordbot.py
@@ -0,0 +1,19 @@
+import discord # インストールした discord.py
+
+client = discord.Client() # 接続に使用するオブジェクト
+
+# 起動時に通知してくれる処理
+@client.event
+async def on_ready():
+    print('ログインしました')
+
+# 「/neko」と発言したら「にゃーん」が返る処理
+@client.event
+async def on_message(message):
+    if message.content.startswith('/neko'):
+        reply = 'にゃーん'
+        await client.send_message(message.channel, reply)
+
+# botの接続と起動
+# （tokenにはbotアカウントのアクセストークンを入れてください）
+client.run('NTM1NjE5NTM1ODAyMzM1MjU0.DyK1nA.DxZ8IeqWta7qqjE8AmUo5z5smAk')
