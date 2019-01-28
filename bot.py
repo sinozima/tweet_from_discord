2995624d63bc192d4b78418faf69eed42745f5c1
sinozima <150318gen@gmail.com>
2019-01-28 23:03:41 +0900

    a

--- bot.py
+++ bot.py
@@ -1,22 +1,23 @@
-import discord # インストールした discord.py
+import discord 
+import tweepy
+client = discord.Client() 
 
-client = discord.Client() # 接続に使用するオブジェクト
-
-# 起動時に通知してくれる処理
 @client.event
 async def on_ready():
     print('ログインしました')
-    reply = 'にゃーん'
-    await client.send_message(message.channel, reply)
-    
 
-# 「/neko」と発言したら「にゃーん」が返る処理
 @client.event
 async def on_message(message):
-    if message.content.startswith('/neko'):
-        reply = 'にゃーん'
-        await client.send_message(message.channel, reply)
+  if message.author != client.user:
+      id = ""+str(message.channel)
+      if id == "twitter":
+        CK = "qQyf2BKpyh39YvR1VpVIOlEZS"
+        CS = "kSGapIP5fK8Hjz23oOv9aOuVqUGiZT2lgTC3f7n2UF6j8DYc8k"
+        auth = tweepy.OAuthHandler(CK,CS)
+        AT = "1086013602262265861-rmxaTBsSTM157alrTYElh3JZHFm70N"
+        AS = "0KAe1lDfA3ZB9BAuN4UdRXYTND8M6isEGDhNxaa7OyHSr"
+        auth.set_access_token(AT,AS)
+        api = tweepy.API(auth)
+        api.update_status(message.content)
 
-# botの接続と起動
-# （tokenにはbotアカウントのアクセストークンを入れてください）
 client.run('NTM1NjE5NTM1ODAyMzM1MjU0.DyK1nA.DxZ8IeqWta7qqjE8AmUo5z5smAk')
